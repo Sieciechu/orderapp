c10137a8fe6683a5df9be926c93de40a85be5d5e
Sieciech <wojciech.mocek@o2.pl>
2019-06-26 10:41:07 +0200

    Created ClientId value object

---
+++ src/main/java/pl/nstrefa/wojciechmocek/domain/ClientId.java
@@ -0,0 +1,37 @@
+package pl.nstrefa.wojciechmocek.domain;
+
+import lombok.NonNull;
+
+import java.util.Objects;
+
+class ClientId {
+    private String value;
+
+    ClientId(@NonNull String value) {
+        if (6 < value.length()) {
+            throw new IllegalArgumentException("ClientId cannot be longer than 6 chars");
+        }
+        if (!value.matches("[A-Za-z0-9ąćęłńóśźżĄĘŁŃÓŚŹŻ ]+")) {
+            throw new IllegalArgumentException("ClientId must contain only alphanumeric chars");
+        }
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ClientId clientId = (ClientId) o;
+        return value.equals(clientId.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
