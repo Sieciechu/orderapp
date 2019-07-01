ac836a737dd014ca9830f8eb284fb91130dbc56e
Sieciech <wojciech.mocek@o2.pl>
2019-07-01 11:17:04 +0200

    Orders repository: added methods

--- src/main/java/pl/nstrefa/wojciechmocek/domain/OrdersRepository.java
+++ src/main/java/pl/nstrefa/wojciechmocek/domain/OrdersRepository.java
@@ -1,5 +1,23 @@
 package pl.nstrefa.wojciechmocek.domain;
 
+import java.util.Set;
+
 public interface OrdersRepository {
-    public void store(Order order);
+    void store(Order order);
+
+    int countAllOrders();
+
+    int countOrdersForCustomer(ClientId clientId);
+
+    double sumPriceofAllOrders();
+
+    double sumPriceOfOrdersForCustomer(ClientId clientId);
+
+    Set<Order> getAll();
+
+    Set<Order> getOrdersForCustomer(ClientId clientId);
+
+    double getAveragePriceOfOrderForCustomer();
+
+    double getAveragePriceOfOrderForCustomer(ClientId clientId);
 }
