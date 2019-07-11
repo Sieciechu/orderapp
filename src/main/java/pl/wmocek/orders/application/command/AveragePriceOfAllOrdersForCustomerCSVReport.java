2ff76ad4fbda3706ebf86f889b47559a85926e92
Sieciech <wojciech.mocek@o2.pl>
2019-07-11 16:37:57 +0200

    removed csv commands&handlers

--- src/main/java/pl/wmocek/orders/application/command/AveragePriceOfAllOrdersForCustomerCSVReport.java
+++
@@ -1,8 +0,0 @@
-package pl.wmocek.orders.application.command;
-
-public class AveragePriceOfAllOrdersForCustomerCSVReport extends Base {
-
-    public AveragePriceOfAllOrdersForCustomerCSVReport(CommandData commandData) {
-        super(commandData);
-    }
-}
